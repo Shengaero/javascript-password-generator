2143de35b1b147e5f760f0d1f20624c9f362e68b
Kaidan <kaidangustave@yahoo.com>
2022-09-01 19:52:26 -0500

    added arrays of different character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// lowercase characters
+var lowerChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// uppercase characters
+var upperChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// numbers
+var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// special characters
+var specialChars = [" ", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
