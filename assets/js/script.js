43dfbbe8e8ad203c7cf08ab1666fe28aa830baab
Kaidan <kaidangustave@yahoo.com>
2022-09-01 20:13:52 -0500

    added function to prompt for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,37 @@ var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 // special characters
 var specialChars = [" ", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
+function getLengthCriteria() {
+  var length = undefined;
+
+  while(length === undefined) {
+    // prompt for user input
+    length = window.prompt("What length should the password be? (8 - 128)", "12");
+    // if not a number, reset length value
+    if(isNaN(length)) {
+      length = undefined;
+    } else {
+      // parse int
+      length = parseInt(length);
+      // if value is outside of predetermined range reset value
+      if(length < 8 || length > 128) {
+        length = undefined;
+      } else {
+        // we have a valid value, break out of loop
+        break;
+      }
+    }
+    // alert we need to try again
+    window.alert("Please select a length between 8 and 128!")
+  }
+
+  return parseInt(length);
+}
+
+function generatePassword() {
+  var length = getLengthCriteria();
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
